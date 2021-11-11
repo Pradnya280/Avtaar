cc74da13dbfa97f105917fbcd67285c4135a3531
Pradnya280 <78024989+Pradnya280@users.noreply.github.com>
2021-11-11 06:38:04 +0530

    Add files via upload

---
+++ avtaar1.py
@@ -0,0 +1,11 @@
+user_name=input("enter your name::")
+print("hi,",user_name,"!")
+
+#random id
+import random
+for x in range(1):
+    print(random.randint(1,1001))
+
+from datetime import datetime
+now=datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
